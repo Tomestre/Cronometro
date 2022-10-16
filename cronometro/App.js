74bf3fbb494849e478fdb430d4f22fe1a8a80bad
Tome <gabrielveleci@gmail.come>
2022-10-16 19:17:49 -0300

    Style e Layout

--- cronometro/App.js
+++ cronometro/App.js
@@ -1,1 +1,27 @@
 import React from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Styles from './Styles.js';
+
+export default function app() {
+  return (
+    <View style={Styles.container}>
+      <Image source={require('./src/crono.png')} />
+
+      <Text style={Styles.timer}>00:00:00</Text>
+
+      <View style={Styles.areabtn}>
+        <TouchableOpacity style={Styles.btn}>
+          <Text style={Styles.btnText}>Vai</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={Styles.btn}>
+          <Text style={Styles.btnText}>Vai</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={Styles.areaUltimo}>
+        <Text style={Styles.textUltimo}>O ultimo tempo foi 00:00:00</Text>
+      </View>
+    </View>
+  );
+}
